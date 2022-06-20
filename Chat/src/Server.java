1a0af5840b9bb861e5043f286bfa6d26c02fcab4
LuizFelipePorto <83476859+LuizFelipePorto@users.noreply.github.com>
2022-06-20 11:27:59 -0300

    Programa funcionando

--- Chat/src/Server.java
+++ Chat/src/Server.java
@@ -2,65 +2,85 @@
 // 1. Execute a classe Server.java
 // 2. Execute Client.java o número de vezes igual a quantos clientes você desejar
 // 3. Mande mensagens com o seguinte formato: mensagem # cliente
+// obs: não utilize Ctrl+Alt+N para rodar as classes, o VS Code não reconhece os arquivos
+
+// Java implementation of Server side
+// It contains two classes : Server and ClientHandler
+// Save file as Server.java
 
 import java.io.*;
 import java.util.*;
 import java.net.*;
 
+// Server class
 public class Server {
 
+	// Vector to store active clients
 	static Vector<ClientHandler> ar = new Vector<>();
 
-	static int clientTotal = 0;
+	// counter for clients
+	static int i = 0;
 
 	public static void main(String[] args) throws IOException {
-
+		// server is listening on port 1234
 		ServerSocket ss = new ServerSocket(1234);
 
 		Socket s;
 
-		// loop infinito para o requisição de cliente
+		// running infinite loop for getting
+		// client request
 		while (true) {
-
+			// Accept the incoming request
 			s = ss.accept();
 
 			System.out.println("New client request received : " + s);
 
+			// obtain input and output streams
 			DataInputStream dis = new DataInputStream(s.getInputStream());
 			DataOutputStream dos = new DataOutputStream(s.getOutputStream());
 
 			System.out.println("Creating a new handler for this client...");
 
-			ClientHandler mtch = new ClientHandler(s, "client " + clientTotal, dis, dos);
+			// Create a new handler object for handling this request.
+			ClientHandler mtch = new ClientHandler(s, "client " + i, dis, dos);
 
+			// Create a new Thread with this object.
 			Thread t = new Thread(mtch);
 
 			System.out.println("Adding this client to active client list");
 
+			// add this client to active clients list
 			ar.add(mtch);
 
+			// start the thread.
 			t.start();
 
-			clientTotal++;
+			// increment i for new client.
+			// i is used for naming only, and can be replaced
+			// by any naming scheme
+			i++;
 
 		}
 	}
 }
 
+// ClientHandler class
 class ClientHandler implements Runnable {
-	Scanner sc = new Scanner(System.in);
+	Scanner scn = new Scanner(System.in);
 	private String name;
 	final DataInputStream dis;
 	final DataOutputStream dos;
 	Socket s;
-	boolean isLoggedIn;
+	boolean isloggedin;
 
+	// constructor
 	public ClientHandler(Socket s, String name,
 			DataInputStream dis, DataOutputStream dos) {
 		this.dis = dis;
 		this.dos = dos;
 		this.name = name;
 		this.s = s;
+		this.isloggedin = true;
 	}
 
 	@Override
@@ -69,25 +89,28 @@ class ClientHandler implements Runnable {
 		String received;
 		while (true) {
 			try {
+				// receive the string
 				received = dis.readUTF();
 
 				System.out.println(received);
 
 				if (received.equals("logout")) {
-					this.isLoggedIn = false;
+					this.isloggedin = false;
 					this.s.close();
 					break;
 				}
 
-				// divide a string em mensagem e recipiente
-				StringTokenizer st = new StringTokenizer(received, " # ");
+				// break the string into message and recipient part
+				StringTokenizer st = new StringTokenizer(received, "#");
 				String MsgToSend = st.nextToken();
 				String recipient = st.nextToken();
 
-				// busca se o recipiente está cadastrado no vetor
+				// search for the recipient in the connected devices list.
+				// ar is the vector storing client of active users
 				for (ClientHandler mc : Server.ar) {
-					// se o recipiente for encontrado, devolve a mensagem assim
-					if (mc.name.equals(recipient) && mc.isLoggedIn == true) {
+					// if the recipient is found, write on its
+					// output stream
+					if (mc.name.equals(recipient) && mc.isloggedin == true) {
 						mc.dos.writeUTF(this.name + " : " + MsgToSend);
 						break;
 					}
@@ -99,6 +122,7 @@ class ClientHandler implements Runnable {
 
 		}
 		try {
+			// closing resources
 			this.dis.close();
 			this.dos.close();
 
